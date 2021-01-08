a17c7286357018244d1d8efc2a2d0c2687ccef43
Jonhatan Hick <jonhatanhick@veronicas-Air.lan>
2021-01-08 22:44:33 +1100

    update division sign

--- public/js/client-side-division.js
+++ public/js/client-side-division.js
@@ -71,22 +71,22 @@ $messageIncorrect.textContent = ''
 const generateQuestion = function (wait) {
     if (questionCount.length<1 && answer<10) {
         setTimeout(() => {
-            $messageOne.textContent = `${valueThree}  /  ${valueOne} = _`
+            $messageOne.textContent = `${valueThree} \xF7 ${valueOne} = _`
     }, wait)
 }
 
     else if (questionCount.length<1 && answer>9) {
         setTimeout(() => {
-            $messageOne.textContent = `${valueThree}  /  ${valueOne} = _ _`
+            $messageOne.textContent = `${valueThree} \xF7 ${valueOne} = _ _`
     }, wait)
     }
 
     else if (questionCount.length>0 && answer<10) {
-        $messageOne.textContent = `${valueThree}  /  ${valueOne} = _`
+        $messageOne.textContent = `${valueThree} \xF7 ${valueOne} = _`
     }
 
     else {
-        $messageOne.textContent = `${valueThree}  /  ${valueOne} = _ _`
+        $messageOne.textContent = `${valueThree} \xF7 ${valueOne} = _ _`
     }
 }
 
@@ -160,8 +160,8 @@ window.addEventListener('keypress', (e) => {
        
                if (answer.toString().length===1&&Number(keysTyped[0])!==answer) {
                    $messageTitle.textContent = 'Incorrect'
-                   $messageIncorrect.textContent = `*${valueThree} / ${valueOne} does not equal ${keysTyped[0]}` 
-                   $messageOne.textContent = `${valueThree} / ${valueOne} = ${answer}`
+                   $messageIncorrect.textContent = `*${valueThree} \xF7 ${valueOne} does not equal ${keysTyped[0]}` 
+                   $messageOne.textContent = `${valueThree} \xF7 ${valueOne} = ${answer}`
                    questionCount.push('I')
                    localStorage.setItem('questionCount', JSON.stringify(questionCount))
                    setTimeout(()=>{
@@ -171,7 +171,7 @@ window.addEventListener('keypress', (e) => {
                    
                if (answer.toString().length===1&&Number(keysTyped[0])==answer) {
                    $messageTitle.textContent = 'Correct'
-                   $messageOne.textContent = `${valueThree} / ${valueOne} = ${answer}`
+                   $messageOne.textContent = `${valueThree} \xF7 ${valueOne} = ${answer}`
                    correctRecord.push('C')
                    questionCount.push('C')                 
                    localStorage.setItem('correctRecord', JSON.stringify(correctRecord))
@@ -184,14 +184,14 @@ window.addEventListener('keypress', (e) => {
 
 
                else if (answer.toString().length===2&&keysTyped.length===1) {
-                   $messageOne.textContent = `${valueThree} / ${valueOne} = ${keysTyped[0]}`
+                   $messageOne.textContent = `${valueThree} \xF7 ${valueOne} = ${keysTyped[0]}`
 
                }
 
                else if (answer.toString().length===2&&keysTyped.length===2&&(10*Number(keysTyped[0])+Number(keysTyped[1]))!==answer) {
                    $messageTitle.textContent = 'Incorrect'
-                   $messageIncorrect.textContent = `*${valueThree} / ${valueOne} does not equal ${keysTyped[0]}${keysTyped[1]}`
-                   $messageOne.textContent = `${valueThree} / ${valueOne} = ${answer}` 
+                   $messageIncorrect.textContent = `*${valueThree} \xF7 ${valueOne} does not equal ${keysTyped[0]}${keysTyped[1]}`
+                   $messageOne.textContent = `${valueThree} \xF7 ${valueOne} = ${answer}` 
                    questionCount.push('I')
                    localStorage.setItem('questionCount', JSON.stringify(questionCount))
                    setTimeout(()=>{
@@ -202,7 +202,7 @@ window.addEventListener('keypress', (e) => {
 
                else if (answer.toString().length===2&&keysTyped.length===2&&(10*Number(keysTyped[0])+Number(keysTyped[1]))==answer) {
                    $messageTitle.textContent = 'Correct'
-                   $messageOne.textContent = `${valueThree} / ${valueOne} = ${answer}`
+                   $messageOne.textContent = `${valueThree} \xF7 ${valueOne} = ${answer}`
                    questionCount.push('C')
                    correctRecord.push('C')
                    localStorage.setItem('correctRecord', JSON.stringify(correctRecord))
